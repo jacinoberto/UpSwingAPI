05d20f6eb1b155c812943b34d1cb1da6b518f3f0
Jacimara Noberto <estudo.jacimaranoberto@gmail.com>
2024-01-26 01:34:54 -0300

    Migration para criar e popular tabela de área de negócios

---
+++ src/main/resources/db/migration/V1__create-table-business-areas.sql
@@ -0,0 +1,6 @@
+CREATE TABLE tb_business_areas(
+    id_business_area UUID NOT NULL,
+    area VARCHAR(75) NOT NULL,
+
+    PRIMARY KEY(id_business_area)
+);
---
+++ src/main/resources/db/migration/V2__insert-into-table-business-areas.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_business_areas VALUES ('2344c5f7-848e-475f-a5d0-eba3a5f2b09c', 'Técnologia');
+INSERT INTO tb_business_areas VALUES ('d26ed7f7-baeb-4f20-b122-ef3f07306644', 'Design');
+INSERT INTO tb_business_areas VALUES ('01c71256-c461-4b36-b672-c8f8d51da3d2', 'Multimídia');
+INSERT INTO tb_business_areas VALUES ('987fb191-9a93-409b-a2d6-cee73c55c5e2', 'Marketing e Venda');
+INSERT INTO tb_business_areas VALUES ('16cff15a-5705-49b5-8bb2-867b960baab3', 'Suporte');
+INSERT INTO tb_business_areas VALUES ('3c967e34-954b-49e3-84dc-95ffef825172', 'Gestão');
+INSERT INTO tb_business_areas VALUES ('e3057003-d5b8-47a9-b2a5-fd1df2ded6f0', 'Finanças e Administração');
+INSERT INTO tb_business_areas VALUES ('4ba6e3b5-ef3c-4af6-8b33-5ef379a4caee', 'Jurídico');
+INSERT INTO tb_business_areas VALUES ('b7ced8b2-98e8-45d7-b05e-b97d5537fedb', 'Engenharia e Manufatura');
