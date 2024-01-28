d4ec60cb2ccb0699655bc0d3d7a4672ac1da072e
Jacimara Noberto <estudo.jacimaranoberto@gmail.com>
2024-01-28 13:48:04 -0300

    Configurando o Cors

---
+++ src/main/java/br/com/noberto/upswing/configurations/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package br.com.noberto.upswing.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Value("${api.security.token.secret}")
+    private String ipSecret;
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "http://" + ipSecret)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "CONNECT", "TRACE", "PATH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
