41a4d49fae9a226a9cad843ac1183fec94cd33d2
Jacimara Noberto <estudo.jacimaranoberto@gmail.com>
2024-01-28 20:44:07 -0300

    Migrations para popular as tabelas zipCode, address e students

---
+++ src/main/resources/db/migration/V13__insert-into-table-zip-codes.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_zip_codes(zip_code, street, area, city, state)
+VALUES ('57040675', 'Travessa Nova', 'Jacintinho', 'Maceió', 'AL'),
+	   ('29023545', 'Rampa José Mirabeau Fernandes', 'Estrelinha', 'Vitória', 'ES'),
+	   ('29052075', 'Escadaria Pio XII', 'Jesus de Nazareth', 'Vitória', 'ES'),
+	   ('29115105', 'Rua Henrique Laranja', 'Paul', 'Vila Velha', 'ES'),
+	   ('29177237', 'Rua Coronel Fabriciano', 'Divinópolis', 'Serra', 'ES'),
+	   ('29179387', 'Rua Miosotis', 'Centro da Serra', 'Serra', 'ES'),
+	   ('29100550', 'Rua Maria Valadares', 'Olaria', 'Vila Velha', 'ES'),
+	   ('29154019', 'Rua Principal', 'Santana', 'Cariacica', 'ES'),
+	   ('29146711', 'Travessa José Vieira', 'Vera Cruz', 'Cariacica', 'ES'),
+	   ('29182430', 'Rua Divino Rossi Vecci', 'Parque das Gaivotas', 'Serra', 'ES');
---
+++ src/main/resources/db/migration/V14__insert-into-table-addresses.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_addresses (id_address, zip_code_id, number, complement)
+VALUES ('b2792001-ed69-471c-b498-4dab88e21e16', '57040675', '562', 'Casa amarela'),
+	   ('8d7f6e98-0d3b-4bca-b9f9-85b9f6fe881e', '29023545', '1478', 'Casa amarela'),
+	   ('9c04e967-6079-40b8-a9fb-5861d41f0ab8', '29052075', '10', 'Casa amarela'),
+	   ('670f5e6a-01eb-4db1-95d0-d2182a854886', '29115105', '5698', 'Casa amarela'),
+	   ('128462cc-8152-4105-9cc8-edc622012893', '29179387', '87', 'Casa amarela'),
+	   ('c52381a1-e790-4a64-9de2-64079c690f03', '29182430', '123', 'Casa amarela'),
+	   ('6a980420-6df4-4a70-afd2-dcd677226eed', '29146711', '227', 'Casa amarela'),
+	   ('20ee0c61-53cb-4294-a51b-d08676a5c75e', '29177237', '987', 'Casa amarela'),
+	   ('c363c8c5-9bc5-4bea-a8e0-033f52720809', '29100550', '1236', 'Casa amarela'),
+	   ('7aee2533-e958-4deb-b704-5a7bad7f188b', '29154019', '885', 'Casa amarela');
