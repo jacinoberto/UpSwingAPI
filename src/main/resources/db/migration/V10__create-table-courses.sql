a386359956a6b34ebf4af88f74d873352f86b160
Jacimara Noberto <estudo.jacimaranoberto@gmail.com>
2024-01-28 15:17:39 -0300

    Migration para criação da tabela de cursos

---
+++ src/main/resources/db/migration/V10__create-table-courses.sql
@@ -0,0 +1,12 @@
+CREATE TABLE tb_courses(
+    id_course UUID,
+    course_name VARCHAR(150) NOT NULL,
+    business_area_id UUID NOT NULL,
+    degree VARCHAR(75) NOT NULL,
+    schedule INT NOT NULL,
+    monthly_cost NUMERIC(6,2),
+    total_cost NUMERIC(6,2),
+
+    PRIMARY KEY(id_course),
+    CONSTRAINT fk_business_area_course_id FOREIGN KEY(business_area_id) REFERENCES tb_business_areas(id_business_area)
+);
